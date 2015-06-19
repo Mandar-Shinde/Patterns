30d5f0f30331c40b481a7e81576b7f7f63dd90be
Mandar <01.mandar@gmail.com>
2015-06-19 23:42:27 +0530

    updated factory methods

--- Creational_Factory/Factory.cpp
+++ Creational_Factory/Factory.cpp
@@ -1,53 +1,77 @@
 
-//Singleton pattern is a design pattern that restricts the instantiation of a class to one object
-// + introduces global state into an application
-// + Single object
+//Factory Method - Define an interface for creating an object, but let subclasses decide which class to instantiate.
+// + Client create an object from abstract class. But the subclass decides which class to instantiate. 
+// + client delegate the decision of choosing the concrete class to create the object to one of the subclasses.
 // 
-// - some consider it an anti - pattern
-// - Memory relesead after program exit
+// - We should specify the exact type of the object at compile time, so that the compiler can allocate memory for that specific type.
+// If we are constructing derived object, the compiler calls the base class constructor first, and the derived class hasn't been initialized yet.
+// This is the reason why we cannot call virtual methods from the constructor.
 
 #include <stdio.h>
 #include <conio.h>
 #include <string>
 #include <iostream>
+#include <vector>
 
 class Product
 {
-	std::string name;
 public:
-	Product(){};
-	Product(std::string arg){ name = arg; }
-	virtual void Job()=0;
+	virtual void Info() = 0;
 };
 
-class Element : public Product
+enum GoogleFactory{ YOUTUBE = 0, GMAIL, GLASS };
+
+class Youtube : public Product
 {
 public:
-	Element(){}
-	Element(std::string name) :Product(name)
+	void Info()
 	{
-	}
-
-	void Job()
-	{
-		printf(" Job of product 1  \n");
+		printf(" Product Info : Youtube \n");
 	}
 };
 
-
-class Production
+class Gmail : public Product
 {
 public:
-	Production() : _warehouse(0)
-	{ 
-		printf(" Production Started  \n");
-	}
-	
-	void MakeProduct(char *name)
+	void Info()
 	{
-		printf(" Making Product [%s] \n", name);		
-		_totalProducts[_warehouse] = Element(name);
-		_warehouse++;
+		printf(" Product Info : Gmail  \n");
+	}
+};
+
+class GoogleGlass : public Product
+{
+public:
+	void Info()
+	{
+		printf(" Product Info : Google Glass  \n");
+	}
+};
+
+
+class Factory
+{
+public:
+	Factory() :_warehouse(0){}
+	Product* MakeProduct(int id)
+	{
+		switch (id)
+		{
+		case YOUTUBE:
+			_warehouse++;
+			return new Youtube();
+			break;
+		case GMAIL:
+			_warehouse++;
+			return new Gmail();
+			break;
+		case GLASS:
+			_warehouse++;
+			return new GoogleGlass();
+			break;
+		default:
+			break;
+		}
 	}
 
 	int GetTotalProducts()
@@ -57,38 +81,24 @@ public:
 
 private:
 	int _warehouse;
-
-	Element _totalProducts[10];
 };
 
-class Factory : public Production
-{
-public:
-	Production* ProductionLine1;
-	Factory()
-	{
-		ProductionLine1 = new Production();
-	}
-	void Produce(char* pname)
-	{
-		printf(" New Product to be created: [%s] \n", pname);
-		ProductionLine1 ->MakeProduct(pname);
-	}
-
-	int TotalProduced()
-	{		
-		return ProductionLine1->GetTotalProducts();
-	}
-
-	
-};
 
 int main()
 {
-	Factory google;
-	google.Produce(" Google Glass ");
-	google.Produce(" Android ");
+	Factory *google = new Factory;
+	Product *prod;
 
-	printf("Total Products By Google: [%d]", google.TotalProduced());
+	prod = google->MakeProduct(YOUTUBE);
+	prod->Info();
+	prod = google->MakeProduct(GMAIL);
+	prod->Info();
+	prod = google->MakeProduct(GLASS);
+	prod->Info();
+	prod = google->MakeProduct(GMAIL);
+	prod->Info();
+
+
+	printf("\n\n Total Products By Google: [%d]", google->GetTotalProducts());
 	_getche();
 }
