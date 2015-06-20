6e6828aa4694ed9fd22e6992a18e824930fbf20e
Mandar <01.mandar@gmail.com>
2015-06-20 09:04:47 +0530

    added Prototype

---
+++ Creational_Prototype/Prototype.cpp
@@ -0,0 +1,9 @@
+
+//Prototype allows you to avoid expensive initialization routines when you construct objects that are very similar.
+//The goal is to minimize the amount of work needed in creating new objects when the initialization routines are expensive.
+
+
+#include <stdio.h>
+#include <conio.h>
+#include <string>
+#include <iostream>
